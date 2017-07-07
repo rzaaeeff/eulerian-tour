589b2d5f0329bd6b04f4486886f58002fac03a4a
rzaaeeff <rzaaeeff@gmail.com>
2017-07-08 00:42:30 +0400

    feat: [Board] Create mode mechanism

--- app/Board.js
+++ app/Board.js
@@ -1,6 +1,24 @@
 "uses strict"
+
+export const MODE_VERTEX = 0;
+export const MODE_EDGE = 1;
+const DEFAULT_MODE = MODE_VERTEX;
+
 export class Board {
     constructor(paper) {
         this.paper = paper;
+
+        this.mode = DEFAULT_MODE;
+    }
+
+    /**
+     * Changes mode.
+     * There are two modes: vertex and edge.
+     * @param newMode new value for mode.
+     */
+    changeMode(newMode) {
+        if (newMode === MODE_VERTEX || newMode === MODE_EDGE) {
+            this.mode = newMode;
+        }
     }
 }
