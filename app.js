65eae3321ab0e690725c8a87e70af32ab6d05909
rzaaeeff <rzaaeeff@gmail.com>
2017-07-08 00:48:38 +0400

    feat: Implement mode mechanism

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-import {Board} from "./app/Board.js";
+import {
+    Board,
+    MODE_VERTEX,
+    MODE_EDGE
+} from "./app/Board.js";
 
 let board = new Board(Raphael("div_board", 1280, 800));
 
 $(document).ready(function() {
-    alert("Testing");
-    board.paper.circle(50, 50, 10);
-    board.paper.circle(65, 50, 10);
-    board.paper.circle(80, 50, 10);
-    board.paper.circle(95, 50, 10);
+    $("#button_modeVertex").click(function(event) {
+        board.changeMode(MODE_VERTEX);
+    });
+
+    $("#button_modeEdge").click(function(event) {
+        board.changeMode(MODE_EDGE);
+    });
 });
